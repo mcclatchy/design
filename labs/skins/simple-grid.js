ea86860979869bfacdea4a075cfbbc0e8ab0c8ec
Jay Pilgreen <jpilgreen@mcclatchy.com>
2020-05-13 11:17:16 -0500

    Publishing to gh-pages

--- labs/skins/simple-grid.js
+++ labs/skins/simple-grid.js
@@ -12,12 +12,37 @@ class SimpleGrid extends HTMLElement {
   get template() {
     let t = document.createElement("template");
     t.innerHTML = `
-    <link rel="stylesheet" href="${this.sds.href}">
-
     <style>
     :host {
       display: block;
     }
+
+    .grid {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      grid-gap: 30px;
+      max-width: 1140px;
+      margin: 30px auto;
+    }
+
+    @media(min-width: 660px) {
+      ::slotted(.photo-lede) {
+        grid-column: 1/-1;
+      }
+
+      ::slotted(.video-lede) {
+        grid-column: 1/3;
+        grid-row: 1/3;
+      }
+      
+      ::slotted(.zone-el) {
+        grid-column: 1;
+        align-self: center;
+        justify-self: center;
+      }
+    }
+
+    /* Dark Theme */
 
     :host([theme=dark]) {
       background-color: #222;
@@ -29,17 +54,10 @@ class SimpleGrid extends HTMLElement {
     :host([theme=dark]) ::slotted(.card) {
       background-color: #373737 !important;
     }
-
-    ::slotted(.card:nth-of-type(20)) {
-      display: none !important;
-    }
-
-    ::slotted(.lead-item) {
-      grid-column: 1/-1;
-    }
     </style>
 
     <slot name="above"></slot>
+    <slot name="nav"></slot>
     <section>
       <slot class="grid"></slot>
     </section>
@@ -69,21 +87,28 @@ class SimpleGrid extends HTMLElement {
     this.shadowRoot.appendChild(this.template.content.cloneNode(true));
 
     // Tweak the lead
-    this.articles[0].classList.add("horizontal", "impact", "in-depth");
+    this.handleLedeStory();
 
     // Append articles
     this.articles.forEach((a, i) => {
       this.appendChild(a);
     });
 
+    // Inject zones
+    this.handleZones();
+
+    // Entry point for extensions
+    this.beforeMove();
+
     // Move this element into position
     this.main.insertAdjacentElement("beforebegin", this);
     this.main.remove();
 
-    // Check for dark theme
-    if(this.theme == "dark") {
-      this.style.sheet.insertRule("body { background-color: #222 }");
-    }
+    // Check for a set theme
+    this.handleTheme();
+
+    // Entry point for extensions 
+    this.beforeShow();
 
     // Unfade
     window.requestAnimationFrame(() => {
@@ -158,7 +183,7 @@ class SimpleGrid extends HTMLElement {
   }
 
   /**
-   * Returns a new style tag in the head for this element
+   * Returns a new style tag in the head labeled for this element
    */
 
   get style() {
@@ -174,11 +199,73 @@ class SimpleGrid extends HTMLElement {
   }
 
   /**
-   * Returns the theme
+   * Modifies the lead story
    */
 
-  get theme() {
-    return this.getAttribute("theme");
+  handleLedeStory() {
+    let lede = this.articles[0];
+
+    if(lede.querySelector(".video") != null) {
+      lede.classList.add("video-lede");
+    } else {
+      this.articles[0].classList.add("photo-lede", "horizontal", "impact", "in-depth");
+    }
+  }
+
+  /**
+   * Makes adjutments based on theme
+   */
+
+  handleTheme() {
+    let theme = this.getAttribute("theme");
+
+    switch(theme) {
+      case "dark":
+        this.style.sheet.insertRule("body { background-color: #222 }");
+        break;
+      default:
+        // Do nothing
+    }
+  }
+
+  /**
+   * Injects zones if specified
+   */
+
+  handleZones() {
+    let zones = this.getAttribute("zones");
+
+    switch(zones) {
+      case "simple":
+        this.insertBefore(this.zone(3), this.articles[4]);
+        this.insertBefore(this.zone(5), this.articles[4]);
+        this.insertBefore(this.zone(6), this.articles[4]);
+        this.insertBefore(this.zone(7), this.articles[8]);
+
+        let z8 = this.zone(8);
+        z8.childNodes[1].hidden = true;
+        z8.classList.add("d-md");
+        this.insertBefore(z8, this.articles[8]);
+        break;
+      default:
+        // Do nothing
+    }
+  }
+
+  /**
+   * Run right before the content is moved
+   */
+
+  beforeMove() {
+    // Made for extending. Do nothing.
+  }
+
+  /**
+   * Runs right before fade is removed
+   */
+
+  beforeShow() {
+    // Made for extending. Do nothing.
   }
 }
 
@@ -187,9 +274,3 @@ class SimpleGrid extends HTMLElement {
  */
 
 customElements.define("simple-grid", SimpleGrid);
-
-/**
- * Export for module
- */
-
-export default SimpleGrid;
