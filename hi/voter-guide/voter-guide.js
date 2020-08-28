8e2f5b92094e861bb1936f7fd6cb1488badb863e
Jay Pilgreen <jpilgreen@mcclatchy.com>
2020-08-28 15:19:28 -0500

    new ad map

--- hi/voter-guide/voter-guide.js
+++ hi/voter-guide/voter-guide.js
@@ -59,42 +59,56 @@ class VoterGuide extends window.SimpleGrid {
 
   dropZone(zone, order) {
     let vb = this.querySelector("voter-ballot");
-    zone.setAttribute("slot", "races");
-    zone.style.order = order;
-    zone.classList.add("vg-zone");
-    vb.appendChild(zone);
+
+    if(zone) {
+      zone.setAttribute("slot", "races");
+      zone.style.order = order;
+      zone.classList.add("vg-zone");
+
+      let z9 = zone.querySelector("#zeus_mn-gpt-9");
+      if(z9) z9.remove();
+
+      vb.appendChild(zone);
+    }
   }
 
   handleZones() {
-    this.addCSS(`
-      #zone-el-2 {
-        display: none !important;
-      }
-    `);
-
     // Move ad test
     if(this.hasAttribute("ads")) {
 
-      zeus.on("NODE_CONNECTED", ele => {
-        if(ele.renderBehavior == "never") {
-          ele.closest(".zone-el").hidden = true;
-        }
-      });
-
-      let z3 = this.getZone(3);
-      this.dropZone(z3, 3);
-
+      // zeus.on("NODE_CONNECTED", ele => {
+      //   if(ele.renderBehavior == "never") {
+      //     ele.closest(".zone-el").hidden = true;
+      //   }
+      // });
+      
+      // Desktop
+      let z2 = document.querySelector("#zone-el-2");
+      this.dropZone(z2, 3)
+      
+      // Tablet
       let z4 = this.getZone(4);
-      this.dropZone(z4, 9);
+      this.dropZone(z4, 3);
 
-      let z5 = this.getZone(5);
-      this.dropZone(z5, 1000);
-
+      // Mobile
       let z6 = this.getZone(6);
-      z6.setAttribute("slot", "");
-      z6.style.order = 1000;
-      z6.classList.add("vg-zone");
-      this.appendChild(z6);
+      this.dropZone(z6, 3);
+
+      // Desktop
+      let z5 = this.getZone(5);
+      this.dropZone(z5, 9);
+
+      // Mobile
+      let z3 = this.getZone(3);
+      this.dropZone(z3, 9);
+
+      // Desktop
+      let z8 = this.getZone(8);
+      this.dropZone(z8, 1000);
+
+      // Mobile
+      let z7 = this.getZone(7);
+      this.dropZone(z7, 1000);
 
       this.addCSS(`
         .ntv-ap {
@@ -103,6 +117,27 @@ class VoterGuide extends window.SimpleGrid {
 
         .vg-zone {
           grid-column: 1/-1;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          min-height: 90px;
+          background-color: blue;
+        }
+
+        voter-guide zeus-ad {
+          display: block !important;
+          width: auto !important;
+        }
+
+        voter-guide zeus-ad::before {
+          content: attr(id);
+          color: white;
+        }
+      `);
+    } else {
+      this.addCSS(`
+        #zone-el-2 {
+          display: none !important;
         }
       `);
     }
