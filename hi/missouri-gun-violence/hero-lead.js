18f7af14915fb5ebeab0340667c372bc56a58b70
Jay Pilgreen <jpilgreen@mcclatchy.com>
2021-01-07 10:54:31 -0600

    registration and free-trial decks

--- hi/missouri-gun-violence/hero-lead.js
+++
@@ -1,3 +0,0 @@
-const leadArt = document.querySelector(".story-body header + figure");
-leadArt.classList.add("full-bleed");
-leadArt.previousElementSibling.insertAdjacentElement("beforebegin", leadArt);
