9c166fb19968203a94b8224a4e81d45047c3e049
Jay Pilgreen <jpilgreen@mcclatchy.com>
2022-03-09 15:16:50 -0600

    adding premium-topper.js changes

--- static/js/premium-topper.js
+++ static/js/premium-topper.js
@@ -1,1 +1,162 @@
-/localhost/prodx/gcp-bucket/prodx/premium-topper/premium-topper.js
+/**
+ * Premium Topper
+ */
+
+class PremiumTopper extends HTMLElement {
+
+  /**
+   * Shadow DOM template
+   */
+
+  get template() {
+    let t = document.createElement("template");
+    t.innerHTML = `
+    <style>
+      :host {
+        display: block;
+        --hf: var(--premium-serif);
+      }
+
+      :host(.loaded) ::slotted(*) {
+        opacity: 1 !important;
+      }
+
+      slot {
+        display: block;
+        margin: 0;
+      }
+
+      .container {
+        display: grid;
+        gap: var(--gap);
+        max-width: var(--section-width);
+        margin: 0 auto;
+      }
+
+      .content {
+        max-width: 450px;
+      }
+
+      .label {
+        font: 400 13px var(--sans);
+        color: var(--premium-impact);
+        text-transform: uppercase;
+        margin: 0;
+      }
+
+      .content::slotted(*) {
+        margin-top: 0;
+      }
+
+      .media::slotted(.pt-video) {
+        position: relative;
+        width: 100%;
+        padding-top: 56.25%;
+      }
+
+      @media(min-width: 768px) {
+        .container {
+          grid-template-columns: 1fr 1fr;
+          grid-template-rows: auto auto 1fr;
+          grid-template-areas:
+            "label label"
+            "headline media"
+            "content media";
+        }
+
+        .label {
+          grid-area: label;
+        }
+
+        .headline {
+          grid-area: headline;
+        }
+        
+        .media {
+          grid-area: media;
+        }
+
+        .content {
+          grid-area: content;
+        }
+      }
+    </style>
+
+    <div class="container grid">
+      <h4 class="label"><b>Your community portal:</b> what you need to know TEST</h4>
+      <slot name="headline" class="headline"></slot>
+      <slot name="media" class="media"></slot>
+      <slot class="content" class="content"></slot>
+    </div>
+    `;
+    return t;
+  }
+
+  /**
+   * Called once when instantiated
+   */
+
+  constructor() {
+    super();
+
+    // Add the Shadow DOM
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+
+    // Fetch the data
+    this.render();
+  }
+
+  /**
+   * Fetches a structure from the ProdX app
+   *
+   * Note: prolly need to check for a bas status message here
+   */
+
+  async render() {
+    const endpoint = `https://storage.googleapis.com/mc-high-impact/prodx/premium-topper/docs/${this.dataset.id}.json`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    
+    // Inject the content
+    this.insertAdjacentHTML("afterbegin", data?.html.portal);
+
+    // Add in the media
+    switch(data?.media?.type) {
+      case "photo":
+        if(data.media.url) {
+          this.insertAdjacentHTML("beforeend", `
+          <figure slot="media">
+            <img src="${data.media.url}">
+            <figcaption>${data.media.caption} <span class="byline">${data.media.photographer.toUpperCase()}</span></figcaption>
+          </figure>`);
+        } else {
+          console.warn("No image source for premium topper");
+        }
+        break;
+      case "video":
+        if(data.media.url) {
+          this.insertAdjacentHTML("beforeend", `
+          <figure slot="media">
+            <div class="embed-video-wrapper">
+              <iframe src="${data.media.url}"></iframe>
+            </div>
+            <figcaption>${data.media.caption} <span class="byline">${data.media.photographer.toUpperCase()}</span></figcaption>
+          </figure>`);
+        }
+        break;
+    }
+
+    // Clean up a bit
+    this.querySelectorAll("h1").forEach(ele => ele.slot="headline");
+
+    // Show it up
+    this.classList.add("loaded");
+  }
+}
+
+// Register the element
+customElements.define("premium-topper", PremiumTopper);
+
+// ES export
+export default PremiumTopper;
