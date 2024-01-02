0cb9d50e848fba7668e3a4b3b52c9c87140275f1
Gabriela Hanna <gabriela.k.hanna@gmail.com>
2024-01-02 13:40:57 -0800

    publishing to gh-pages

--- js/navigation.js
+++ js/navigation.js
@@ -2,15 +2,26 @@
  * Sample placeholder
  */
 
-function showNavigation() {
-  document.querySelector("#main-nav").classList.add("open");
-  document.body.classList.add("freeze");
-}
+// Set menu button to click
+var navButton = document.getElementById('menu-toggle');
 
-function closeNavigation() {
-  document.querySelector("#main-nav").classList.remove("open");
-  document.body.classList.remove("freeze");
-}
+// Click the button
+navButton.onclick = function() {
+	
+  // Toggle class "opened". Set also aria-expanded to true or false. Toggle #main-nav class "open".
+  if ( -1 !== navButton.className.indexOf('opened') ) {
+    navButton.className = navButton.className.replace( ' opened', '' );
+    navButton.setAttribute( 'aria-expanded', 'false' );
+    document.querySelector("#main-nav").classList.remove("open");
+    document.body.classList.remove("freeze");
+  } else {
+    navButton.className += ' opened';
+    navButton.setAttribute( 'aria-expanded', 'true' );
+    document.querySelector("#main-nav").classList.add("open");
+    document.body.classList.add("freeze");
+   }
+    
+ };
 
 function toggleSearchForm() {
   document.querySelectorAll(".flag").forEach(d => {
