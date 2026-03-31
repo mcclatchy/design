8f6f82866c6359e56d1b0fa96ae44e32e980dff6
Gabriela Hanna <gabriela.k.hanna@gmail.com>
2026-03-31 15:57:58 -0700

    publishing to gh-pages

--- js/navigation.js
+++ js/navigation.js
@@ -45,7 +45,45 @@ function toggleSubMenu() {
 }
 
 function toggleMSB() {
-  document.documentElement.classList.toggle("msb");
+  const body = document.documentElement;
+  const blackLogo = document.querySelector(".flag-logo a img[src*='/img/sacramento-bee-black.svg']");
+  const whiteLogo = document.querySelector(".flag-logo a img[src*='/img/sacramento-bee-white.svg']");
+
+  // Non-sub elements
+  const nonSubLogin = document.querySelector("#logIn");
+  const nonSubSeparator = document.querySelector(".pipe-seperator");
+  const nonSubSubscribe = document.querySelector(".subscribe-link");
+
+  // Sub elements
+  const subExpanderButton = document.querySelector(".flag-account .expander");
+  const subPopout = document.querySelector("#popout");
+  
+  body.classList.toggle("msb");
+  
+  if (body.classList.contains('msb')) {
+    // If 'msb' class is present (subscriber mode)
+    if (whiteLogo) whiteLogo.style.display = 'block';
+    if (blackLogo) blackLogo.style.display = 'none';
+
+    if (nonSubLogin) nonSubLogin.style.display = 'none';
+    if (nonSubSeparator) nonSubSeparator.style.display = 'none';
+    if (nonSubSubscribe) nonSubSubscribe.style.display = 'none';
+
+    if (subExpanderButton) subExpanderButton.style.display = 'block'; // Assuming default display is block for button
+    if (subPopout) subPopout.style.display = 'block'; // Assuming default display is block for div
+
+  } else {
+    // If 'msb' class is not present (non-subscriber mode)
+    if (whiteLogo) whiteLogo.style.display = 'none';
+    if (blackLogo) blackLogo.style.display = 'block';
+
+    if (nonSubLogin) nonSubLogin.style.display = 'block';
+    if (nonSubSeparator) nonSubSeparator.style.display = 'inline'; // Assuming default display is inline for span
+    if (nonSubSubscribe) nonSubSubscribe.style.display = 'block';
+
+    if (subExpanderButton) subExpanderButton.style.display = 'none';
+    if (subPopout) subPopout.style.display = 'none';
+  }
 }
 
 function showNavigation() {
